9e7e063658e93361975a79546377db973df13382
tongkn <hellotkn@163.com>
2016-10-27 23:29:23 +0800

    tried to write my own containers like lists and map

---
+++ src/main/java/lists/Hello.java
@@ -0,0 +1,10 @@
+package lists;
+
+/**
+ * Created by xiaox on 2016/10/27.
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("this is a test");
+    }
+}
